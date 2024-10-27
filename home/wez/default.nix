35ca8120006fe5d7183f58512a83014da954d8e6
James Maes <james@kof22.com>
2024-10-27 09:30:35 -0500

    Initial working version

---
+++ home/wez/default.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.modules.terminals.wezterm;
+in {
+  options.modules.terminals.wezterm = {
+    enable = mkEnableOption "enable wezterm terminal emulator";
+  };
+
+  config = mkIf cfg.enable {
+    programs.wezterm = {
+      enable = true;
+      package = pkgs.wezterm-nightly;
+      extraConfig = builtins.readFile ./config.lua;
+    };
+  };
+}
+
+
+
+
+
+
+
+
+
+
