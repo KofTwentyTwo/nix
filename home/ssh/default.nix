d8d670f62cc8edde5149fc779c6ba811a7ba7f9a
James Maes <james@kof22.com>
2025-03-04 12:10:38 -0600

    Added missing local hosts, sorted hosts

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -61,18 +61,19 @@
             ###########################
             ## Galaxy Synology Hosts ##
             ###########################
-            emperor   = { port = 10022; hostname = "emperor.galaxy.lan";    user = "EmperorGalaxyAdmin"; };
             coruscant = { port = 10022; hostname = "coruscant.galaxy.lan";  user = "CoruscantGalaxyAdmin"; };
+            emperor   = { port = 10022; hostname = "emperor.galaxy.lan";    user = "EmperorGalaxyAdmin"; };
             scarif    = { port = 10022; hostname = "scarif.galaxy.lan";     user = "ScarifGalaxyAdmin"; };
 
             ############################################
             ## Synology Business Mail / Corp VM Hosts ##
             ############################################
+            galaxy-dsm        = { port = 10022; hostname = "galaxy-dsm.galaxy.lan";       user = "GalaxyDSMGalaxyAdmin"; };
+            kingsrook-dsm     = { port = 10022; hostname = "kingsrook-dsm.galaxy.lan";    user = "KingsrookGalaxyAdmin"; };
             koftwentytwo-dsm  = { port = 10022; hostname = "koftwentytwo-dsm.galaxy.lan"; user = "Kof22GalaxyAdmin"; };
             mmlt-dsm          = { port = 10022; hostname = "mmlt-dsm.galaxy.lan";         user = "MMLTGalaxyAdmin"; };
-            kingsrook-dsm     = { port = 10022; hostname = "kingsrook-dsm.galaxy.lan";    user = "KingsrookGalaxyAdmin"; };
+            qrun-dsm          = { port = 10022; hostname = "qrun-dsm.galaxy.lan";         user = "james.maes"; };
             queuedl-dsm       = { port = 10022; hostname = "queuedl-dsm.galaxy.lan";      user = "QUEUEDLAdmin"; };
-            galaxy-dsm        = { port = 10022; hostname = "galaxy-dsm.galaxy.lan";       user = "GalaxyDSMGalaxyAdmin"; };
             rcid-dsm          = { port = 10022; hostname = "rcid-dsm.galaxy.lan";         user = "RCIDAdmin"; };
 
             ##########################
@@ -81,8 +82,8 @@
             p1  = { port = 10022; hostname = "p1.galaxy.lan";  user = "james.maes"; };
             p2  = { port = 10022; hostname = "p2.galaxy.lan";  user = "james.maes"; };
             p3  = { port = 10022; hostname = "p3.galaxy.lan";  user = "james.maes"; };
-            pmg = { port = 10022; hostname = "pmg.galaxy.lan"; user = "local_admin"; };
             pbs = { port = 10022; hostname = "pbs.galaxy.lan"; user = "local_admin"; };
+            pmg = { port = 10022; hostname = "pmg.galaxy.lan"; user = "local_admin"; };
 
             ##########################
             ## Galaxy Network Hosts ##
@@ -92,39 +93,49 @@
             ##########################
             ## galaxy k8s prod plan ##
             ##########################
-            k8s-prod-controller-lb  = { port = 10022; hostname = "k8s-prod-controller-lb.k8s.galaxy.lan";   user = "james.maes"; };
             k8s-prod-controller-a   = { port = 10022; hostname = "k8s-prod-controller-a.k8s.galaxy.lan";    user = "james.maes"; };
             k8s-prod-controller-b   = { port = 10022; hostname = "k8s-prod-controller-b.k8s.galaxy.lan";    user = "james.maes"; };
             k8s-prod-controller-c   = { port = 10022; hostname = "k8s-prod-controller-c.k8s.galaxy.lan";    user = "james.maes"; };
+            k8s-prod-controller-lb  = { port = 10022; hostname = "k8s-prod-controller-lb.k8s.galaxy.lan";   user = "james.maes"; };
+            k8s-prod-worker-a-1     = { port = 10022; hostname = "k8s-prod-worker-a-1.k8s.galaxy.lan";      user = "james.maes"; };
+            k8s-prod-worker-a-2     = { port = 10022; hostname = "k8s-prod-worker-a-2.k8s.galaxy.lan";      user = "james.maes"; };
+            k8s-prod-worker-b-1     = { port = 10022; hostname = "k8s-prod-worker-b-1.k8s.galaxy.lan";      user = "james.maes"; };
+            k8s-prod-worker-b-2     = { port = 10022; hostname = "k8s-prod-worker-b-2.k8s.galaxy.lan";      user = "james.maes"; };
+            k8s-prod-worker-c-1     = { port = 10022; hostname = "k8s-prod-worker-c-1.k8s.galaxy.lan";      user = "james.maes"; };
+            k8s-prod-worker-c-2     = { port = 10022; hostname = "k8s-prod-worker-c-2.k8s.galaxy.lan";      user = "james.maes"; };
 
             ############################
             ## galaxy wordpress hosts ##
             ############################
             wordpress-galaxy     = { port = 10022; hostname = "wordpress-galaxy.galaxy.lan";    user = "local_admin"; };
-            wordpress-queuedl    = { port = 10022; hostname = "wordpress-queuedl.galaxy.lan";   user = "local_admin"; };
-            wordpress-kof22      = { port = 10022; hostname = "wordpress-kof22.galaxy.lan";     user = "local_admin"; };
             wordpress-kingsrook  = { port = 10022; hostname = "wordpress-kingsrook.galaxy.lan"; user = "local_admin"; };
-            wordpress-rcid       = { port = 10022; hostname = "wordpress-rcid.galaxy.lan";      user = "local_admin"; };
+            wordpress-kof22      = { port = 10022; hostname = "wordpress-kof22.galaxy.lan";     user = "local_admin"; };
             wordpress-mmlt       = { port = 10022; hostname = "wordpress-mmlt.galaxy.lan";      user = "local_admin"; };
+            wordpress-queuedl    = { port = 10022; hostname = "wordpress-queuedl.galaxy.lan";   user = "local_admin"; };
+            wordpress-rcid       = { port = 10022; hostname = "wordpress-rcid.galaxy.lan";      user = "local_admin"; };
 
             ####################
             ## Galaxy.Lan VMs ##  
             ####################
-            ansible-controller   = { port = 10022; hostname = "ansible-controller.galaxy.lan";  user = "local_admin"; };
-            checkmk              = { port = 10022; hostname = "checkmk.galaxy.lan";  user = "local_admin"; };
-            graylog              = { port = 10022; hostname = "graylog.galaxy.lan";  user = "local_admin"; };
-            influx-db1           = { port = 10022; hostname = "influx-db1.galaxy.lan";  user = "local_admin"; };
-            logs                 = { port = 10022; hostname = "logs.galaxy.lan";  user = "local_admin"; };
-            nagios               = { port = 10022; hostname = "nagios.galaxy.lan";  user = "local_admin"; };
-            prod-db1             = { port = 10022; hostname = "prod-db1.galaxy.lan";  user = "local_admin"; };
-            prod-db2             = { port = 10022; hostname = "prod-db2.galaxy.lan";  user = "local_admin"; };
+            ansible-controller     = { port = 10022; hostname = "ansible-controller.galaxy.lan";      user = "local_admin";   };
+            ca                     = { port = 10022; hostname = "ca.galaxy.lan";                      user = "james.maes";    };
+            checkmk                = { port = 10022; hostname = "checkmk.galaxy.lan";                 user = "local_admin";   };
+            coturn                 = { port = 10022; hostname = "coturn.galaxy.lan";                  user = "james.maes";    };
+            dba                    = { port = 10022; hostname = "dba.galaxy.lan";                     user = "james.maes";    };
+            docker-registry-mirror = { port = 10022; hostname = "docker-registry-mirror.galaxy.lan";  user = "local_admin";   };
+            graylog                = { port = 10022; hostname = "graylog.galaxy.lan";                 user = "local_admin";   };
+            influx-db1             = { port = 10022; hostname = "influx-db1.galaxy.lan";              user = "local_admin";   };
+            logs                   = { port = 10022; hostname = "logs.galaxy.lan";                    user = "local_admin";   };
+            pg-db-a-1              = { port = 10022; hostname = "pg-db-a-1.galaxy.lan";               user = "james.maes";    };
+            prod-db1               = { port = 10022; hostname = "prod-db1.galaxy.lan";                user = "local_admin";   };
+            prod-db2               = { port = 10022; hostname = "prod-db2.galaxy.lan";                user = "local_admin";   };
 
             #############################
             ## Galaxy.Lan Workstations ##
             #############################
-            grogu       = { port = 10022; hostname = "grogu.galaxy.lan";        user = "james.maes"; };
-            darth       = { port = 10022; hostname = "darth.galaxy.lan";        user = "james.maes"; };
             count-dooku = { port = 10022; hostname = "count-dooku.galaxy.lan";  user = "james.maes"; };
+            darth       = { port = 10022; hostname = "darth.galaxy.lan";        user = "james.maes"; };
+            grogu       = { port = 10022; hostname = "grogu.galaxy.lan";        user = "james.maes"; };
             jakku       = { port = 10022; hostname = "jakku.galaxy.lan";        user = "james.maes"; };
          };
       };
